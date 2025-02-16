6ea49c67fae4ba39607ea9d1c9b13e0a35ba2c1a
hifichevymane <blekbjek@gmail.com>
2025-02-16 18:22:24 +0200

    add cors support

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 // @deno-types="npm:@types/express@5.0.0"
 import express from "npm:express@4.21.2";
 import { Response } from "npm:express@4.21.2";
+// @deno-types="npm:@types/cors@2.8.17"
+import cors from "npm:cors@2.8.5";
 import "@std/dotenv/load";
 import routes from "./routes/index.ts";
 
@@ -8,6 +10,11 @@ const APP_PORT = Number(Deno.env.get('APP_PORT')) || 8000;
 
 const app = express();
 
+const corsOptions: cors.CorsOptions = {
+  origin: Deno.env.get('FRONTEND_ALLOWED_URL')
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/v1', routes);
 
