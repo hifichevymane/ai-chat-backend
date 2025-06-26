98378317eee58dee5cbc809906006eaf3b813fba
hifichevymane <blekbjek@gmail.com>
2025-06-26 21:21:31 +0300

    throw 404 error if chat is not found

--- src/controllers/chats/show.ts
+++ src/controllers/chats/show.ts
@@ -6,9 +6,15 @@ export const show = async (req: Request, res: Response): Promise<void> => {
   try {
     const chatService = new ChatService();
     const chat = await chatService.getChatById(id);
+
+    if (!chat) {
+      res.status(404).json({ error: `The chat with id ${id} was not found` });
+      return;
+    }
+
     res.status(200).json(chat);
   } catch (err) {
     console.error(err);
-    res.status(404).json({ error: `The record with id ${id} was not found` });
+    res.status(500).json({ error: 'Internal Error' });
   }
 };
