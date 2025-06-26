d9ef5cc368c484fee953d2c115fb2112e9d8b7f8
hifichevymane <blekbjek@gmail.com>
2025-06-26 20:06:13 +0300

    add create user endpoint and user service

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import chats from './chats';
+import users from './users';
 
 const router = Router();
 
 router.use('/chats', chats);
+router.use('/users', users);
 
 export default router;
---
+++ src/services/user.ts
@@ -0,0 +1,54 @@
+import bcrypt from 'bcryptjs';
+import { prisma } from '../database';
+import type { User } from '../database/prisma/src/generated/prisma';
+
+interface CreateUserDTO {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+interface UpdateUserDTO {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export class UserService {
+  public async createUser(user: CreateUserDTO): Promise<User> {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        ...user,
+        password: hashedPassword
+      }
+    });
+    return newUser;
+  }
+
+  public findUserByEmail(email: string): Promise<User | null> {
+    return prisma.user.findUnique({
+      where: { email }
+    });
+  }
+
+  public findUserById(id: string): Promise<User | null> {
+    return prisma.user.findUnique({
+      where: { id }
+    });
+  }
+
+  public deleteUser(id: string): Promise<User> {
+    return prisma.user.delete({
+      where: { id }
+    });
+  }
+
+  public updateUser(id: string, user: UpdateUserDTO): Promise<User> {
+    return prisma.user.update({
+      where: { id },
+      data: user
+    });
+  }
+}
