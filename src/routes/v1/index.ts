128ccdbbc36601b7da3c170be2fd7bce41a2ff3d
hifichevymane <blekbjek@gmail.com>
2025-07-06 13:42:11 +0300

    don't need users create endpoint

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import chats from './chats';
-import users from './users';
 import auth from './auth';
 
 const router = Router();
 
 router.use('/chats', chats);
-router.use('/users', users);
 router.use('/auth', auth);
 
 export default router;
