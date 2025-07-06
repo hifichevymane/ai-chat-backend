73aeafb61b200e7e8b88d5a2dce4845c16278bcf
hifichevymane <blekbjek@gmail.com>
2025-07-06 13:06:56 +0300

    validate users endpoints

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { create } from '../../controllers/users';
+import { validateBody } from '../../middlewares';
+import { createUserBodySchema } from '../../controllers/users/schemas';
 
 const router = Router();
 
-router.post('/', create);
+router.post('/', validateBody(createUserBodySchema), create);
 
 export default router;
