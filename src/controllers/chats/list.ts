c34786753d9b626b184cc4afc9347c65d8adf0ac
Maksym Rumu <106327285+hifichevymane@users.noreply.github.com>
2025-07-06 13:56:57 +0300

    specific user chats (#19)

--- src/controllers/chats/list.ts
+++ src/controllers/chats/list.ts
@@ -1,8 +1,15 @@
 import type { Request, Response } from 'express';
 import { ChatService } from '../../services';
+import { HttpError } from '../http-error';
 
-export const list = async (_: Request, res: Response): Promise<void> => {
+export const list = async (req: Request, res: Response): Promise<void> => {
+  if (!req.user) {
+    throw new HttpError(401, 'Unauthorized');
+  }
+
   const chatService = new ChatService();
-  const chats = await chatService.getAllChats();
+  const userId = req.user.id;
+  const chats = await chatService.getAllChats(userId);
+
   res.status(200).json(chats);
 };
