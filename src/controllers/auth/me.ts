99d51663062e838a044fc44d81cd09038de28d15
Maksym Rumu <106327285+hifichevymane@users.noreply.github.com>
2025-07-12 16:48:57 +0300

    auth/me endpoint to fetch a valid user information (#21)

---
+++ src/controllers/auth/me.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from 'express';
+import { HttpError } from '../http-error';
+
+export const me = (req: Request, res: Response): void => {
+  if (!req.user) {
+    throw new HttpError(401, 'Unauthorized');
+  }
+
+  const { user } = req;
+  console.log(user);
+  res.json(user);
+};
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { login, verify, logout, signUp } from '../../controllers/auth';
-import { validateBody } from '../../middlewares';
+import { login, verify, logout, signUp, me } from '../../controllers/auth';
+import { validateBody, authenticateJWT } from '../../middlewares';
 import {
   loginBodySchema,
   verifyBodySchema,
@@ -13,5 +13,6 @@ router.post('/login', validateBody(loginBodySchema), login);
 router.post('/logout', logout);
 router.post('/sign-up', validateBody(signUpBodySchema), signUp);
 router.post('/verify', validateBody(verifyBodySchema), verify);
+router.get('/me', authenticateJWT, me);
 
 export default router;
