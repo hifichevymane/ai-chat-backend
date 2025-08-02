63d94b8ec0de83895c430c157797bb493cdb63f1
hifichevymane <blekbjek@gmail.com>
2025-08-02 14:51:10 +0300

    clear refreshToken cookie with the correct options in the logout endpoint

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -4,7 +4,7 @@ import { HttpError } from '../http-error';
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
   const token = req.headers.authorization?.split(' ')[1];
-  const refreshToken = (req.cookies as Record<string, string | undefined>)
+  const refreshToken = (req.signedCookies as Record<string, string | undefined>)
     .refreshToken;
 
   if (!token || !refreshToken) {
@@ -15,6 +15,11 @@ export const logout = async (req: Request, res: Response): Promise<void> => {
   await authService.blacklistJWT(token);
   await authService.blacklistJWT(refreshToken);
 
-  res.clearCookie('refreshToken');
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    signed: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict'
+  });
   res.status(204).end();
 };
